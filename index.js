a3468ef25ff08be80bd88c6c32a6de9582fff9ca
niftylettuce <niftylettuce@gmail.com>
2018-06-20 23:42:07 -0500

    Removed label

--- index.js
+++ index.js
@@ -11,15 +11,10 @@ function BrowserStdout(opts) {
 
   opts = opts || {}
   WritableStream.call(this, opts)
-  this.label = (opts.label !== undefined) ? opts.label : 'stdout'
 }
 
 BrowserStdout.prototype._write = function(chunks, encoding, cb) {
   var output = chunks.toString ? chunks.toString() : chunks
-  if (this.label === false) {
-    console.log(output)
-  } else {
-    console.log(this.label+':', output)
-  }
+  console.log(output)
   process.nextTick(cb)
 }
